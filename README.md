b42ac5c0fd8012a3a1c47e6759833d0b0c09f266
hamishkendall <hamishkendall@yahoo.com>
2019-05-03 18:08:59 +1200

    ASP.NET core setup with VUE
    Was so easy

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Swamp Team - PlantATreeCo Web Application
 
 > A web application powered by Vue.js and ASP.net
+> Tests were completed
---
+++ wwwroot/js/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app dark>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Plant-A-Tree-Co</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>
+            {{item.icon}}
+          </v-icon>
+          {{item.title}}
+        </v-btn>
+
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
+        </v-list-tile>
+
+
+      </v-list>
+    </v-navigation-drawer>
+
+
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        {icon: 'home', title:'Home'},
+        {icon: 'info', title:'About'},
+        {icon: 'shopping_cart', title:'Shop'},
+        {icon: 'face', title:'Sign Up'},
+        {icon: 'lock_open', title:'Sign In'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
