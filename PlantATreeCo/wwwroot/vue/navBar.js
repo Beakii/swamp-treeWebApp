2563a1aabda3f640adcb3c36099c5f224ef13f1d
James <auminist3@gmail.com>
2019-05-06 16:02:21 +1200

    Navbar and MobileNav toggle working as expected

---
+++ PlantATreeCo/wwwroot/vue/navBar.js
@@ -0,0 +1,20 @@
+﻿var app = new Vue({
+    el: '#layout',
+    data: {
+        drawerToggle: false,
+        menuItems: [
+            { icon: 'home', title: 'Home', link: 'https://www.google.com/' },
+            { icon: 'info', title: 'About', link: 'http://www.gooogle.com' },
+            { icon: 'shopping_cart', title: 'Shop', link: 'google.com' },
+            { icon: 'face', title: 'Sign Up', link: '/about' },
+            { icon: 'lock_open', title: 'Sign In', link: '' }
+        ],
+        
+    },
+
+    methods: {
+        linkTo: function (menuItems) {
+            window.location.href = menuItems.link;
+        }
+    },
+})
